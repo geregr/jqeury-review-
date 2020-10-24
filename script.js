3160a5381f64652998ee25fc4f2744fcd20a7268
geregr <rockyliu20110221@gmail.com>
2020-10-24 11:41:21 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+$("#lol").css("background-color","black")
+
+$("#red").click(()=>{
+    $("#lol").css("background-color","red")
+})
+
+$("#blue").click(()=>{
+    $("#lol").css("background-color","blue")
+})
+
+$("#hide").click(()=>{
+    $("#lol").hide()
+})
+
+$("#show").click(()=>{
+    $("#lol").show()
+})
+
+var width= 100;
+var height = 100
+
+$("#big").click(()=>{
+width+=20
+height+=20
+$("#lol").css("width", width + "px")
+$("#lol").css("height", height + "px")
+
+})
+
+$("#small").click(()=>{
+width-=20
+height-=20
+$("#lol").css("width", width + "px")
+$("#lol").css("height", height + "px")
+
+})
